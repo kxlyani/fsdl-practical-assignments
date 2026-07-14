14943888d40e22651cb8918283509608cd1618f8
kxlyani <patil.kalyani1612@gmail.com>
2026-07-14 21:56:53 +0530

    Fix vite base path for Vercel deployment

--- docs/vite.config.js
+++ docs/vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: '/fsdl-practical-assignments/',
+  // base: '/fsdl-practical-assignments/',
 });
